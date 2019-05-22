01c2b0b72485341d844836924496ba4717f885be
Miller <millerraycell@gmail.com>
2019-05-22 18:21:56 -0400

    Descrição, exemplos e implementação do Algoritmo do barbeiro
    Questão 3 do laboratório do sistemas operacionais que pede a implementação e exemplos do algoritmo do barbeiro

---
+++ Questão 3/barber_algorithm.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define CADEIRAS 8
+
+int clientes_esperando = 0;
+
+sem_t barber;
+sem_t customer;
+sem_t mutex;
+
+void *cliente()
+{
+    sem_wait(&mutex);
+    if(clientes_esperando < CADEIRAS)
+    {
+        customer_arrived();
+        clientes_esperando++;
+        sem_post(&customer);
+        sem_post(&mutex);
+        sem_wait(&barber);
+        get_haircut();
+    }
+    else
+    {
+        sem_post(&mutex);
+        customer_arrived();
+        giveup_haircut();
+    }
+    pthread_exit(NULL);
+}
+
+void *barbeiro()
+{
+    while(1)
+    {
+        sem_wait(&customer);
+        sem_wait(&mutex);
+        clientes_esperando--;
+        sem_post(&barber);
+        sem_post(&mutex);
+        cut_hair();
+    }
+    pthread_exit(NULL);
+}
+
+void cut_hair()
+{
+    printf("Barbeiro esta cortando o cabelo\n");
+    for(int i=0; i< 0xFFFFFFFF; i++);;
+}
+
+void customer_arrived()
+{
+    printf("Cliente chegou para cortar cabelo\n");
+}
+void get_haircut()
+{
+    printf("Cliente esta tendo o cabelo cortado\n");
+}
+
+void giveup_haircut()
+{
+    printf("Cliente desistiu, salao cheio\n");
+}
+
+int main()
+{
+    sem_init(&customer,1,0);
+    sem_init(&barber,1,0);
+    sem_init(&mutex,1,1);
+
+
+    pthread_t barb,clien;
+
+    pthread_create(&barb,NULL,barbeiro,NULL);
+
+    while(1)
+    {
+        pthread_create(&clien,NULL,cliente,NULL);
+        printf("Numero de pessoas na fila de espera: %d\n",clientes_esperando);
+        for(int i=0; i< 0xFFFFFF; i++);
+    }
+
+    return 0;
+}
+
